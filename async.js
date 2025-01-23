d6862882df4085fff1363b6499ae80a0deee482c
Ankitabhamidimarri <meisineutopia@gmail.com>
2025-01-23 10:35:33 +0530

    async.js

--- async.js
+++ async.js
@@ -1,4 +1,4 @@
-console.log(10);
+/*console.log(10);
 console.log(20);
 //setTimeout(()=>{console.log(30)},5000) // console.l0g(30); //? Syntax : setTimeout(()=>{callback} , time)
 console.log(40);
@@ -15,7 +15,7 @@ console.log(50);
 //console.log(p1);
 //})
 
-let p2 = new Promise((resolve , reject)=>{
+/*let p2 = new Promise((resolve , reject)=>{
 resolve("Success!");
 });
 console.log(p2);
@@ -25,14 +25,14 @@ p2.then((response)=>{
     console.log(error);
 }).finally(()=>{
     console.log("Finally is printing for both!");
-})
+})*/
 
 /*console.log(p2);
 p2.then((response)=>{
     console.log(response);
     })*/
 
-let p3 = new Promise((resolve , reject)=>{
+/*let p3 = new Promise((resolve , reject)=>{
 reject("Failure!");
 })
 console.log(p3);
@@ -57,7 +57,7 @@ console.log(p3);
 //^method chaining can include both then and catch
 
 // ! Api fetching
-function fetchUsers(){
+/*function fetchUsers(){
 let res = window.fetch("https://jsonplaceholder.typicode.com/users")
 //console.log(res);
 res.then(val => {
@@ -83,5 +83,32 @@ res.then(val => {
 }).catch(err => {
     console.log(err);
 })
+}
+fetchUsers();*/
+
+/*let p = new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve("Promise Successfully complteing");
+    }, 3000)
+})
+async function demo(){
+    /*console.log("hello");
+    console.log("hello");
+    console.log("bye");   
+    console.log("10");
+    console.log("20"); 
+    console.log("30");*/
+    /*console.log("Start");
+    let x =await p;
+    console.log(x);
+
+    console.log("End");
+}
+demo();*/
+
+async function fetchUsers(){
+    let response= await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    console.log(data);
 }
 fetchUsers();
